c012ad7e1e7313e1bdcacbf99d96b7a135a430b9
he <slut@vip.qq.com>
2022-02-14 17:06:55 +0800

    add ch1

---
+++ javascript/ch1/helloword.js
@@ -0,0 +1,15 @@
+let x = 1;
+console.log(x);
+
+let book = {
+    title: "javascript",
+    edition: 7
+}
+console.log(book);
+
+let arr = [1, 2, 3];
+console.log(arr);
+
+//箭头函数=>，用来分割参数列表和函数体
+const plus=x=>x+1;
+console.log(plus(1));
